763cd0b7563b4d30e33aefdd7122a178859238ed
hamish <hamish@679a26be-e637-0410-a801-b7d8065a4221>
2011-06-13 18:44:37 +0000

    set media prefix correctly for apache
    git-svn-id: https://svn.aptivate.org/svn/idsapi/dev@5189 679a26be-e637-0410-a801-b7d8065a4221

--- django/idsapi/settings.py
+++ django/idsapi/settings.py
@@ -46,12 +46,13 @@ STATIC_ROOT = ''
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/static/'
+STATIC_URL = '/media/'
 
 # URL prefix for admin static files -- CSS, JavaScript and images.
 # Make sure to use a trailing slash.
 # Examples: "http://foo.com/static/admin/", "/static/admin/".
-ADMIN_MEDIA_PREFIX = '/static/admin/'
+ADMIN_MEDIA_PREFIX = '/media/'
+#ADMIN_MEDIA_PREFIX = '/static/admin/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
