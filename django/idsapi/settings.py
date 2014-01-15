c2ce693d9832ea4e417228661889dc7ccb40daa2
hamish <hamish@679a26be-e637-0410-a801-b7d8065a4221>
2014-01-15 17:57:37 +0000

    Fixes for new deploy environment and django version
    - new location for admin static/media files
    - new path to urls conf
    - change db used by staging_new
    
    git-svn-id: https://svn.aptivate.org/svn/idsapi/dev@11075 679a26be-e637-0410-a801-b7d8065a4221

--- django/idsapi/settings.py
+++ django/idsapi/settings.py
@@ -554,7 +554,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
 )
 
-ROOT_URLCONF = 'idsapi.urls'
+ROOT_URLCONF = 'urls'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
