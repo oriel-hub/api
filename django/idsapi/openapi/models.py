e949ef859f3ab11fb0b603e1dbd0aeac759ef969
Hamish Downer <hamish@aptivate.org>
2014-02-27 16:32:54 +0000

    monkeypatch sunburnt's SolrString to not escape spaces
    It is unnecessary, and causes some queries to not work as expected.
    
    And having done this, we can get rid of the to_rawstring() method.
    
    Conflicts:
    	django/idsapi/openapi/search_builder.py

--- django/idsapi/openapi/models.py
+++ django/idsapi/openapi/models.py
@@ -1,3 +1,11 @@
-#from django.db import models
+# put start up code here
 
-# Create your models here.
+# Here we work around aparent limitations in sunburnt's escaping. The
+# definition of SolrString always escapes whitespace, something that
+# doesn't seem needed, nor is correctly handled by the SOLR server
+# itself, so we remove whitespace from lucene_special_chars by
+# monkeypatching SolrString
+
+from sunburnt.strings import SolrString
+
+SolrString.lucene_special_chars = '+-&|!(){}[]^"~*?:\t\v\\/'
