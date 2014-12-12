6f4bfd72fcde79a51aac06eecf6c007c685dcc4c
Peter Mason <p.mason@ids.ac.uk>
2014-12-12 12:45:14 +0000

    Update urls.py
    Update url to match new admin paths in application

--- django/idsapi/userprofile/urls.py
+++ django/idsapi/userprofile/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls.defaults import patterns, url
 import userprofile.admin
 
 urlpatterns = patterns('',
-    url(r'^admin/user/download/$', userprofile.admin.download_view,
+    url(r'^apiadmin/user/download/$', userprofile.admin.download_view,
         name='user_list_download'),
 )
