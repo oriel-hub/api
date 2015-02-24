24b32e69dff4598b95a1445723e10a02cbd6a2f4
Hamish Downer <hamish@aptivate.org>
2015-02-24 15:20:36 +0000

    don't let the url_info context_processor kill tests
    by having a default value

--- django/idsapi/openapi/context_processors.py
+++ django/idsapi/openapi/context_processors.py
@@ -1,6 +1,9 @@
+from django.contrib.sites.models import Site
+
+
 def url_info(request):
     from .defines import URL_ROOT
     return {
-        'domain_name': request.META['HTTP_HOST'],
+        'domain_name': request.META.get('HTTP_HOST', Site.objects.get(id=1).domain),
         'url_root': URL_ROOT,
     }
