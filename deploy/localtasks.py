c2581b7614762b82a1196c48eefc1c768690ec36
Hamish Downer <hamish@aptivate.org>
2015-07-23 11:14:15 +0100

    have jenkins tasks running with and without integration
    to get feedback on test coverage without integrations tests

--- deploy/localtasks.py
+++ deploy/localtasks.py
@@ -1,9 +1,11 @@
 import os
 import tasklib
+from .project_settings import django_apps
+
 
 def deploy(environment=None):
     """Do all the required steps in order"""
-    if environment == None:
+    if environment is None:
         environment = tasklib._infer_environment()
         if tasklib.env['verbose']:
             print "Inferred environment as %s" % environment
@@ -13,6 +15,7 @@ def deploy(environment=None):
     tasklib.create_ve()
     tasklib.update_db()
     update_docs()
+
 
 def update_docs():
     """Generate the documentation using sphinx"""
@@ -25,3 +28,38 @@ def update_docs():
     if tasklib.env['quiet']:
         docs_env['SPHINXOPTS'] = '-q'
     tasklib._call_wrapper(['make', 'html'], cwd=docs_dir, env=docs_env)
+
+
+def _manage_py_jenkins(apps_to_test):
+    """ run the jenkins command """
+    args = ['jenkins', ]
+    args += ['--pylint-rcfile', os.path.join(tasklib.env['project_dir'], 'jenkins', 'pylint.rc')]
+    coveragerc_filepath = os.path.join(tasklib.env['project_dir'], 'jenkins', 'coverage.rc')
+    if os.path.exists(coveragerc_filepath):
+        args += ['--coverage-rcfile', coveragerc_filepath]
+    args += apps_to_test
+    if not tasklib.env['quiet']:
+        print "### Running django-jenkins, with args; %s" % args
+    tasklib._manage_py(args, cwd=tasklib.env['project_dir'])
+
+
+def run_jenkins(apps_to_test=None):
+    """ make sure the local settings is correct and the database exists """
+    if apps_to_test is None:
+        apps_to_test = django_apps
+    tasklib.env['verbose'] = True
+    tasklib.update_ve()
+    tasklib._install_django_jenkins()
+    tasklib.create_private_settings()
+    tasklib.link_local_settings('jenkins')
+    tasklib.clean_db()
+    tasklib.update_db()
+    _manage_py_jenkins(apps_to_test)
+
+
+def run_jenkins_fast():
+    run_jenkins(['openapi', 'userprofile'])
+
+
+def run_jenkins_full():
+    run_jenkins(['openapi', 'userprofile', 'openapi_integration'])
