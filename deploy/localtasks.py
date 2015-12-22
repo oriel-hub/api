7f71156ce5276f7ddde0fd1d7edb5f5b1d1a2c3e
tomd <tomd+git@aptivate.org>
2015-12-22 17:34:56 +0000

    Upgrade core dependencies WIP
    (leaves in broken state)

--- deploy/localtasks.py
+++ deploy/localtasks.py
@@ -1,18 +1,13 @@
+from __future__ import unicode_literals, absolute_import
+
 import os
 import tasklib
+from project_settings import django_apps
 
-def deploy(environment=None):
-    """Do all the required steps in order"""
-    if environment == None:
-        environment = tasklib._infer_environment()
-        if tasklib.env['verbose']:
-            print "Inferred environment as %s" % environment
 
-    tasklib.create_private_settings()
-    tasklib.link_local_settings(environment)
-    tasklib.create_ve()
-    tasklib.update_db()
+def post_deploy(environment):
     update_docs()
+
 
 def update_docs():
     """Generate the documentation using sphinx"""
@@ -25,3 +20,55 @@ def update_docs():
     if tasklib.env['quiet']:
         docs_env['SPHINXOPTS'] = '-q'
     tasklib._call_wrapper(['make', 'html'], cwd=docs_dir, env=docs_env)
+
+
+def _install_django_jenkins():
+    """ ensure that pip has installed the django-jenkins thing """
+    if not tasklib.env['quiet']:
+        print "### Installing Jenkins packages"
+    pip_bin = os.path.join(tasklib.env['ve_dir'], 'bin', 'pip')
+    cmds = [
+        # django-jenkins after 0.14 require django>=1.4, so pin to 0.14
+        [pip_bin, 'install', 'django-jenkins==0.14'],
+        [pip_bin, 'install', 'pylint==1.3'],
+        [pip_bin, 'install', 'astroid==1.2.1'],
+        [pip_bin, 'install', 'coverage']]
+
+    for cmd in cmds:
+        tasklib._call_wrapper(cmd)
+
+
+def _manage_py_jenkins(apps_to_test):
+    """ run the jenkins command """
+    args = ['jenkins', ]
+    args += ['--pylint-rcfile', os.path.join(tasklib.env['project_dir'], 'jenkins', 'pylint.rc')]
+    coveragerc_filepath = os.path.join(tasklib.env['project_dir'], 'jenkins', 'coverage.rc')
+    if os.path.exists(coveragerc_filepath):
+        args += ['--coverage-rcfile', coveragerc_filepath]
+    args += apps_to_test
+    if not tasklib.env['quiet']:
+        print "### Running django-jenkins, with args; %s" % args
+    tasklib._manage_py(args, cwd=tasklib.env['project_dir'])
+
+
+def run_jenkins(apps_to_test=None, local_settings='jenkins'):
+    """ make sure the local settings is correct and the database exists """
+    if apps_to_test is None:
+        apps_to_test = django_apps
+    tasklib.env['verbose'] = True
+    tasklib.update_ve()
+    _install_django_jenkins()
+    tasklib.create_private_settings()
+    tasklib.link_local_settings(local_settings)
+    tasklib.clean_db()
+    tasklib.update_db()
+    _manage_py_jenkins(apps_to_test)
+
+
+def run_jenkins_fast():
+    fast_django_apps = [a for a in django_apps if 'integration' not in a]
+    run_jenkins(fast_django_apps, 'jenkins_fast')
+
+
+def run_jenkins_full():
+    run_jenkins()
--- django/idsapi/manage.py
+++ django/idsapi/manage.py
@@ -1,95 +1,65 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from os import path
-import shutil, sys, subprocess
+from __future__ import unicode_literals, absolute_import
 
-# check python version is high enough
-MIN_PYTHON_MINOR_VERSION = 6
-if sys.version_info[0] != 2:
-    print >> sys.stderr, "Arrggh - not using python 2.x"
-    sys.exit(1)
-if sys.version_info[1] < MIN_PYTHON_MINOR_VERSION:
-    print >> sys.stderr, "You must use python 2.%d or later, you are using 2.%d" % (
-            MIN_PYTHON_MINOR_VERSION, sys.version_info[1])
-    sys.exit(1)
+import os
+import sys
+from os import path
 
 PROJECT_ROOT = path.abspath(path.dirname(__file__))
-REQUIREMENTS = path.abspath(path.join(PROJECT_ROOT, '..', '..', 'deploy', 'pip_packages.txt'))
-VE_ROOT = path.join(PROJECT_ROOT, '.ve')
-VE_TIMESTAMP = path.join(VE_ROOT, 'timestamp')
+DEPLOY_DIR = path.abspath(path.join(PROJECT_ROOT, os.pardir, os.pardir, 'deploy'))
+sys.path.append(DEPLOY_DIR)
 
-ve_dir_mtime = path.exists(VE_ROOT) and path.getmtime(VE_ROOT) or 0
-ve_timestamp_mtime = path.exists(VE_TIMESTAMP) and path.getmtime(VE_TIMESTAMP) or 0
-reqs_timestamp = path.getmtime(REQUIREMENTS)
+# ignore the usual virtualenv
+# note that for runserver Django will start a child process, so that it
+# can kill and restart the child process. So we use the environment to pass
+# the argument along.
+if '--ignore-ve' in sys.argv:
+    sys.argv.remove('--ignore-ve')
+    os.environ['IGNORE_DOTVE'] = 'true'
 
-def ve_dir_older_than_reqs():
-    return ve_dir_mtime < reqs_timestamp
+# VIRTUAL_ENV will be set if we are already inside a virtualenv (either because
+# of standard virtualenv activate, or because go_to_ve() set it).
+if 'IGNORE_DOTVE' not in os.environ and 'VIRTUAL_ENV' not in os.environ:
+    import ve_mgr
 
-def ve_timestamp_older_than_reqs():
-    return ve_timestamp_mtime < reqs_timestamp
+    # check python version is high enough
+    ve_mgr.check_python_version(2, 6, __file__)
 
-def update_ve_timestamp():
-    file(VE_TIMESTAMP, 'w').close()
+    # if it appears that the virtualenv is out of date then stop here
+    updater = ve_mgr.UpdateVE()
+    if updater.virtualenv_needs_update():
+        print "VirtualEnv needs to be updated"
+        print 'Run deploy/bootstrap.py'
+        sys.exit(1)
 
-def go_to_ve():
-    """
-    If running inside virtualenv already, then just return and carry on.
-
-    If not inside the virtualenv then call the virtualenv python, pass it
-    this file and all the arguments to it, so this file will be run inside
-    the virtualenv.
-    """
-    if not sys.prefix == VE_ROOT:
-        if sys.platform == 'win32':
-            python = path.join(VE_ROOT, 'Scripts', 'python.exe')
-        else:
-            python = path.join(VE_ROOT, 'bin', 'python')
-
-        retcode = subprocess.call([python, __file__] + sys.argv[1:])
-        sys.exit(retcode)
-
-# manually update virtualenv?
-update_ve = 'update_ve' in sys.argv or 'update_ve_quick' in sys.argv
-# destroy the old virtualenv so we have a clean virtualenv?
-destroy_old_ve = 'update_ve' in sys.argv
-
-# we've been told to update the virtualenv
-if update_ve:
-    # if we need to create the virtualenv, then we must do that from
-    # outside the virtualenv. The code inside this if statement will only
-    # be run outside the virtualenv.
-    if destroy_old_ve and path.exists(VE_ROOT):
-        shutil.rmtree(VE_ROOT)
-    if not path.exists(VE_ROOT):
-        import virtualenv
-        virtualenv.logger = virtualenv.Logger(consumers=[])
-        #virtualenv.create_environment(VE_ROOT, site_packages=True)
-        virtualenv.create_environment(VE_ROOT, site_packages=False)
-
-    # install the pip requirements and exit
-    pip_path = path.join(VE_ROOT, 'bin', 'pip')
-    pip_retcode = subprocess.call([pip_path, 'install',
-            '--requirement=%s' % REQUIREMENTS ])
-    if pip_retcode == 0:
-        update_ve_timestamp()
-    sys.exit(pip_retcode)
-# else if it appears that the virtualenv is out of date:
-elif ve_dir_older_than_reqs() or ve_timestamp_older_than_reqs():
-    print "VirtualEnv need to be updated"
-    print 'Run "./manage.py update_ve" (or "./manage.py update_ve_quick")'
-    sys.exit(1)
-
-# now we should enter the virtualenv. We will only get
-# this far if the virtualenv is up to date.
-go_to_ve()
+    # now we should enter the virtualenv. We will only get
+    # this far if the virtualenv is up to date.
+    updater.go_to_ve(__file__, sys.argv[1:])
 
 # run django - the usual manage.py stuff
-from django.core.management import execute_manager
-try:
-    import settings # Assumed to be in the same directory.
-except ImportError:
-    sys.stderr.write("Error: Can't find the file 'settings.py' in the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n(If the file settings.py does indeed exist, it's causing an ImportError somehow.)\n" % __file__)
-    sys.exit(1)
-
 if __name__ == "__main__":
-    execute_manager(settings)
+    sys.path.append(DEPLOY_DIR)
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+
+    try:
+        import settings  # pylint: disable=W402
+    except ImportError as e:
+        raise ImportError("%s\n\nFailed to import settings module: "
+            "does it contain errors? Did you run tasks.py deploy:dev?"
+            % e)
+
+    # {% if cookiecutter.use_pytest == 'yes' %}
+    # we're using pytest so treat tests as special
+    if 'test' in sys.argv:
+        import pytest
+        # We need to remove "test" so that pytest picks up additional args only
+        sys.argv.pop(1)
+        sys.exit(pytest.main())
+    else:
+        from django.core.management import execute_from_command_line
+        execute_from_command_line(sys.argv)
+    # {% else %}
+    from django.core.management import execute_from_command_line
+    execute_from_command_line(sys.argv)
+    # {% endif %}
