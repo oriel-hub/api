5a6667b1f6a3eea4240af61c49145c0669804315
tomd <tomd+git@aptivate.org>
2015-12-22 17:36:57 +0000

    Django upgrade fixes
    - url module import rename
    - drop deprecated URLField option
    
    (cherry picked from commit 2d64806b6bf22bc28341d7ea621ae87ef74e949a)

--- django/idsapi/openapi/urls.py
+++ django/idsapi/openapi/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls.defaults import patterns, url
+from django.conf.urls import patterns, url
 
 from openapi.views import AllObjectView, ObjectSearchView, ObjectView, FieldListView, \
     RootView, FacetCountView, CategoryChildrenView, The404View
