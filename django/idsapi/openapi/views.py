866ceadd20d158cbe1b6a280f85d216b619f83bb
tomd <tomd+git@aptivate.org>
2015-12-30 14:34:02 +0000

    Rest framework upgrade fixes: session auth
    Use new session based auth naming

--- django/idsapi/openapi/views.py
+++ django/idsapi/openapi/views.py
@@ -1,5 +1,5 @@
 from rest_framework import status
-from rest_framework.authentication import UserLoggedInAuthentication
+from rest_framework.authentication import SessionAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import View
@@ -62,7 +62,7 @@ class RootView(View):
 
 class BaseAuthView(View):
     permissions = (IsAuthenticated, PerUserThrottlingRatePerGroup)
-    authentication = (GuidAuthentication, UserLoggedInAuthentication)
+    authentication = (GuidAuthentication, SessionAuthentication)
 
     def __init__(self):
         View.__init__(self)
