b04098a7b7781e9507d7e6927c18b90ba7586b32
tomd <tomd+git@aptivate.org>
2016-02-16 15:34:11 +0000

    Set STATIC_ROOT
    (cherry picked from commit a201536f458b6772c8244c162a7fca1507e4ed1a)

--- django/idsapi/settings.py
+++ django/idsapi/settings.py
@@ -13,6 +13,7 @@ DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
 PROJECT_HOME = os.path.dirname(os.path.realpath(__file__))
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 ALLOWED_HOSTS = [
     '.api.ids.ac.uk',
@@ -56,7 +57,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
