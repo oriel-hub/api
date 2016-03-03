fedb6254f48e16f395592072c811b3fffee37e18
tomd <tomd+git@aptivate.org>
2016-03-03 15:59:19 +0000

    Remove the tracking middleware
    We'll track directly by hooking into API views.

--- django/idsapi/settings.py
+++ django/idsapi/settings.py
@@ -110,7 +110,6 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'server_tracking.django.middleware.PageViewMiddleware',
 )
 
 ROOT_URLCONF = 'urls'
@@ -221,6 +220,10 @@ LOGGING = {
             'level': 'ERROR',
             'propagate': True,
         },
+        'openapi.analytics': {
+            'handlers': ['console'],
+            'level': 'INFO',
+        }
     }
 }
 
