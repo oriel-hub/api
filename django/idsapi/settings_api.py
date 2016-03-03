47daa565573da026f5bc5d38bf96f3312d6a13f3
tomd <tomd+git@aptivate.org>
2016-03-03 14:13:21 +0000

    Point IDSAPI v2 SOLR config to jabba

--- django/idsapi/settings_api.py
+++ django/idsapi/settings_api.py
@@ -23,8 +23,8 @@ DEFAULT_SITE = 'eldis'
 
 if SERVER_ENV == "production":
     SOLR_SERVER_URLS = {
-        'eldis': 'http://localhost:8983/solr/eldis-live/',
-        'bridge': 'http://localhost:8983/solr/bridge-live/',
+        'eldis': 'http://jabba.ids.ac.uk:8983/solr/eldis-live/',
+        'bridge': 'http://jabba.ids.ac.uk:8983/solr/bridge-live/',
     }
 elif SERVER_ENV in ["staging", "localdev"]:
     SOLR_SERVER_URLS = {
