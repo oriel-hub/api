4e289aa2199387f712bfbd0b64b32e9cb413e74d
tomd <tomd+git@aptivate.org>
2019-07-22 02:14:28 +0100

    Use new style django render context

--- django/idsapi/userprofile/views.py
+++ django/idsapi/userprofile/views.py
@@ -1,8 +1,9 @@
 from django.core.urlresolvers import reverse
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
-from django.shortcuts import render_to_response
+from django.shortcuts import render
 from django.template import RequestContext
+
 
 @login_required
 def profile_detail(request):
@@ -10,13 +11,13 @@ def profile_detail(request):
     profile_obj = request.user.userprofile
     if profile_obj.agree_to_licensing == False:
         return HttpResponseRedirect(reverse('edit_profile'))
-    context = RequestContext(request)
+    context = {}
     context['first_name'] = request.user.first_name
     context['last_name'] = request.user.last_name
     context['email'] = request.user.email
 
-    return render_to_response('profiles/profile_detail.html',
-                              { 'profile': profile_obj },
-                              context_instance=context)
+    return render(request, 'profiles/profile_detail.html', {
+        'profile': profile_obj,
+        'context': context})
 
 
