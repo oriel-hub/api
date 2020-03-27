2933bf3cb5e4d995579f1d59124abcaed2cc975f
tomd <tomd+git@aptivate.org>
2020-03-27 06:20:03 +0000

    Use newstyle admin urls

--- django/idsapi/urls.py
+++ django/idsapi/urls.py
@@ -21,7 +21,7 @@ urlpatterns += [
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', admin.site.urls),
 
     url(r'^accounts/', include('django_registration.backends.activation.urls')),
     url(r'^accounts/', include('django.contrib.auth.urls')),
