3656f5f3dd63d315cd467b7394069ec05587ca65
tomd <tomd+git@aptivate.org>
2020-11-05 16:23:58 +0000

    Update available_fields script to support Python3
    documentation generation helper

--- docs/available_fields.py
+++ docs/available_fields.py
@@ -1,15 +1,15 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import unicodecsv
 
 with open('available_fields.csv', 'rb') as f:
     reader = unicodecsv.reader(f)
-    header_row = reader.next()
-    
+    header_row = next(reader)
+
     assert header_row[0] == 'Field name'
     assert header_row[10] == 'User tier restriction'
     assert header_row[11] == 'Description'
-    
+
     with open('available_fields.rst', 'w') as out:
         out.write('=================\nAvailable Fields\n=================\n\n')
         for i, row in enumerate(reader):
@@ -17,9 +17,9 @@ with open('available_fields.csv', 'rb') as f:
                 restrict = row[10]
                 name = row[0]
                 if restrict.lower().find('admin only') > -1:
-                    print "ignoring 'admin only' field: %s" % name
+                    print("ignoring 'admin only' field: %s" % name)
                 elif len(name) == 0:
-                    print "Ignoring row %d with no field name" % (i + 2)
+                    print("Ignoring row %d with no field name" % (i + 2))
                 else:
                     out.write(name + '\n')
                     desc = row[11]
@@ -27,7 +27,7 @@ with open('available_fields.csv', 'rb') as f:
                         desc = "No description"
                     out.write('  ' + desc + '\n\n')
             else:
-                print "Ignoring row %d with %d columns" % (i + 2, len(row))
-        
- 
- 
+                print("Ignoring row %d with %d columns" % (i + 2, len(row)))
+
+
+
